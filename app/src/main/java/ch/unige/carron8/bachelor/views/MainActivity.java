d4fd357feca8537b1560bf78b98890b4af929ab8
Blaise <carron8@etu.unige.ch>
2015-05-31 04:08:49 +0200

    Add devices controller interface activity

--- app/src/main/java/ch/unige/carron8/bachelor/views/MainActivity.java
+++ app/src/main/java/ch/unige/carron8/bachelor/views/MainActivity.java
@@ -100,6 +100,9 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
+        }else if(id == R.id.action_controller){
+            startActivity(new Intent(this, ControllerActivity.class));
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
